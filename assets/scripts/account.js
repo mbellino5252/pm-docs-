5bd2a14074d63f055290d0e5e0010686b60d6133
Vaughan Logan <vlogan1@babson.edu>
2025-05-03 13:02:58 -0400

    account edit form

--- assets/scripts/account.js
+++ assets/scripts/account.js
@@ -1,20 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const profileContainer = document.querySelector("#profilecontainer")
+	const profileContainer = document.querySelector("#profilecontainer");
 
 	fetch("/assets/data/user.json")
 		.then((response) => {
 			if (!response.ok) {
-				throw new Error("Failed to fetch user.json")
+				throw new Error("Failed to fetch user.json");
 			}
-			return response.json()
+			return response.json();
 		})
 		.then((user) => {
-            profileContainer.querySelector(".profileusername").innerHTML = user.username;
-            profileContainer.querySelector(".profilename").innerHTML = `${user.fname} ${user.lname}`;
-            profileContainer.querySelector(".profileskill").innerHTML = user.skilllevel;
-            profileContainer.querySelector(".profilebio").innerHTML = user.bio;
+			profileContainer.querySelector(
+				".profileusername"
+			).innerHTML = `@${user.username}`;
+			profileContainer.querySelector(
+				".profilename"
+			).innerHTML = `${user.fname} ${user.lname}`;
+			profileContainer.querySelector(".profileskill").innerHTML =
+				user.skilllevel;
+			profileContainer.querySelector(".profilebio").innerHTML = user.bio;
 		})
 		.catch((error) => {
-			console.error("Error loading profile:", error)
-		})
-})
+			console.error("Error loading profile:", error);
+		});
+});
+
+const editContainer = document.querySelector("#profileeditcontainer");
+const editForm = document.querySelector("#profileeditform");
+const editBtn = document.querySelector("#editbutton");
+const saveBtn = document.querySelector("#savebutton");
+
+editBtn.addEventListener("click", function () {
+	editContainer.style.display = "grid";
+	editBtn.style.display = "none";
+});
+
+saveBtn.addEventListener("click", function () {
+	editContainer.style.display = "none";
+	editBtn.style.display = "block";
+	// save edited profile data to user.json
+});
